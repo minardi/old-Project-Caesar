023421ca6d8cdae0e400c3c0a68a7c783a6812c6
Kararin <charms_devil@mail.ru>
2015-09-13 20:27:53 +0300

    Schedule: Events Cloning view inheritanse deleted

--- client/js/app/Schedule/tpl/WeekModeTpl.js
+++ client/js/app/Schedule/tpl/WeekModeTpl.js
@@ -1,6 +1,6 @@
 templates.weekModeTpl = _.template([
 '	<div class="weekModeCaption">Weeks mode: All Events</div>',
-'	<div class="btn-group">',
+'	<div>',
 '   	<button type="button" class="allEvents btn btn-primary">All events</button>',
 '  		<button type="button" class="selectedEvent btn btn-primary">Selected Event</button>',
 '	</div>'].join(''));
