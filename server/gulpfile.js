44b0c709f8b69a73de98c92d6682eeac5eb3fca4
unknown <dima.selezen@rambler.ru>
2015-09-22 15:16:00 +0300

    Fixed bug with project building

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('fonts', function () {
 
 gulp.task('img', function () {
     return gulp.src('../client/img/*')
-        .pipe(filter('*.{jpg,png,ico}'))
+        .pipe(filter('*.{jpg,png,ico,JPG}'))
         .pipe(flatten())
         .pipe(gulp.dest(path + '/img'));
 });
